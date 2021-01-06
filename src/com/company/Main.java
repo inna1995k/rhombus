b5bd19f761c6b23a831ee0f8a52770bc15a87f56
inna <basketboll95@yandex.ru>
2021-01-06 16:36:01 +0300

    change

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,44 +1,59 @@
 package com.company;
 
-//5x5
-
-import java.util.Scanner;
-
 public class Main {
 
     public static void main(String[] args) {
-        int height = 5;
-        int width = 5;
+        int height =11;
+        int width = 6;
         Main.printDiamond(height, width);
     }
 
     private static void printDiamond(int height, int width) {
-        int size = height + width;
-        
         System.out.println("Romb");
-        int z = 1;
-        for (int i = 0; i < size / 2; i++) {
-            int prob = (size - z) / 2;
-            for (int j = 0; j < prob; j++) {
-                System.out.print("-");
+        int up = 1;
+        for(int i=height/2; i>=1; i--) {
+            for (int j = i; j >= 1; j--) {//отступы пустые по горизонтали
+                System.out.print(" ");
             }
-            for (int k = 0; k < z; k++) {
+            System.out.print("*");
+            for (int j = 0; j <= up - 2; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j <= up - 2; j++) {
+                System.out.print(" ");
+            }
+
+            if (up != 1) {
                 System.out.print("*");
             }
-            System.out.println();
-            z = z + 2;
+            up++;
+            System.out.print("\n");
         }
-        z = size -1;
-        for (int i = size / 2; i > 0; i--) {
-            int prob = (size - z) / 2;
-            for (int j = 0; j < prob; j++) {
-                System.out.print("-");
+
+
+        int low = height/2 -1;
+        for(int i=1; i<=height/2 -1; i++){//i=1
+
+            for(int j=0; j<=i; j++){
+                System.out.print(" ");
             }
-            for (int k = 0; k < z; k++) {
+            System.out.print("*");
+
+            for(int j=0; j<=low-2; j++){
+                System.out.print(" ");
+            }
+
+            for(int j=0; j<=low-2; j++){
+                System.out.print(" ");
+            }
+
+            if(low!=1){
                 System.out.print("*");
             }
-            System.out.println();
-            z = z - 2;
+
+            low--;
+            System.out.print("\n");
         }
     }
 }
